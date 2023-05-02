eab55dc153168640ab2ee00c517890fc801eb3bd
AtharvaCM <60681622+AtharvaCM@users.noreply.github.com>
2023-05-02 10:41:19 +0530

    [NP-21] CMS: Create schema for Footer on Sanity (#4)
    * - Added header schema
    
    * - Added footer schema
    
    ---------
    
    Co-authored-by: Atharva-CM <atharva.mahamuni@bluepineapple.io>

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,7 +1,8 @@
 import { SchemaTypeDefinition } from 'sanity'
 
+import footer from './schemas/footer'
 import header from './schemas/header'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [header],
+  types: [header, footer],
 }
