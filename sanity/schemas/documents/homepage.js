2960818642f796be0808e8159909fc0971e0a8d7
AtharvaCM <necrons999@gmail.com>
2023-06-10 15:21:15 +0530

    - Added hero and textWithIllustration to homepage

--- sanity/schemas/documents/homepage.js
+++ sanity/schemas/documents/homepage.js
@@ -1,9 +1,11 @@
 /* eslint-disable import/no-anonymous-default-export */
 import gallery from '../common/gallery'
+import hero from '../common/hero'
+import textWithIllustration from '../common/textWithIllustration'
 
 export default {
   name: 'homepage',
   type: 'document',
   title: 'Homepage',
-  fields: [gallery],
+  fields: [gallery, hero, textWithIllustration],
 }
