068bd5634a203ccd0886e36d455e4b566c835970
AtharvaCM <necrons999@gmail.com>
2023-06-10 15:34:15 +0530

    - heroWithCTA type added

--- sanity/schemas/documents/homepage.js
+++ sanity/schemas/documents/homepage.js
@@ -1,11 +1,12 @@
 /* eslint-disable import/no-anonymous-default-export */
 import gallery from '../common/gallery'
 import hero from '../common/hero'
+import heroWithCTA from '../common/heroWithCTA'
 import textWithIllustration from '../common/textWithIllustration'
 
 export default {
   name: 'homepage',
   type: 'document',
   title: 'Homepage',
-  fields: [gallery, hero, textWithIllustration],
+  fields: [gallery, hero, textWithIllustration, heroWithCTA],
 }
