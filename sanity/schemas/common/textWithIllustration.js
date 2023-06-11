c28b64b2b26702596cd7e996963a8a0b08cf48f2
AtharvaCM <60681622+AtharvaCM@users.noreply.github.com>
2023-06-11 16:58:46 +0530

    Release v1.1.0-2023-06-11 (#9)
    * [TECH] Setup codebase and repos for Nishad Pune website (#1)
    
    - Added PR template
    
    Co-authored-by: Atharva-CM <atharva.mahamuni@bluepineapple.io>
    
    * [NP-11] PR temlpate fix (#3)
    
    * - Added PR template
    
    * - Modified PR template
    
    * - Fixed project name
    
    ---------
    
    Co-authored-by: Atharva-CM <atharva.mahamuni@bluepineapple.io>
    Co-authored-by: Atharva <112168596+Atharva-CM@users.noreply.github.com>
    
    * [NP-20] CMS: Create schema for Header on Sanity (#2)
    
    - Added header schema
    
    Co-authored-by: Atharva-CM <atharva.mahamuni@bluepineapple.io>
    
    * [NP-21] CMS: Create schema for Footer on Sanity (#4)
    
    * - Added header schema
    
    * - Added footer schema
    
    ---------
    
    Co-authored-by: Atharva-CM <atharva.mahamuni@bluepineapple.io>
    
    * [NP-12] CMS: Create schema for HomePage on Sanity (#8)
    
    * - Gallery added and changed structure
    
    * - Added common comopnents
    
    * - Added hero and textWithIllustration to homepage
    
    * - heroWithCTA type added
    
    ---------
    
    Co-authored-by: Atharva-CM <atharva.mahamuni@bluepineapple.io>
    
    ---------
    
    Co-authored-by: Atharva-CM <atharva.mahamuni@bluepineapple.io>
    Co-authored-by: Atharva <112168596+Atharva-CM@users.noreply.github.com>

---
+++ sanity/schemas/common/textWithIllustration.js
@@ -0,0 +1,39 @@
+/* eslint-disable import/no-anonymous-default-export */
+export default {
+  name: 'textWithIllustration',
+  type: 'object',
+  title: 'Text with Illustration',
+  fields: [
+    {
+      name: 'heading',
+      type: 'string',
+      title: 'Heading',
+    },
+    {
+      name: 'tagline',
+      type: 'string',
+      title: 'Tagline',
+    },
+    {
+      name: 'excerpt',
+      type: 'text',
+      title: 'Excerpt',
+    },
+    {
+      name: 'image',
+      type: 'image',
+      title: 'Image',
+      options: {
+        hotspot: true,
+      },
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative text',
+          validation: (Rule) => Rule.required(),
+        },
+      ],
+    },
+  ],
+}
