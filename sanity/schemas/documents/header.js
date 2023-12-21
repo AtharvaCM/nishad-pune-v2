f1bf9d8d0ab79d46d7cd21c7a374f451fdd69fbb
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2023-12-21 16:52:17 +0530

    - Added support for nested nav links

--- sanity/schemas/documents/header.js
+++ sanity/schemas/documents/header.js
@@ -1,37 +1,135 @@
 /* eslint-disable import/no-anonymous-default-export */
 export default {
   name: 'header',
-  type: 'document',
   title: 'Header',
+  type: 'document',
   fields: [
     {
-      name: 'navBrandLogo',
+      name: 'logo',
+      title: 'Logo',
       type: 'image',
-      title: 'Navbar Brand Logo',
       options: {
-        hotspots: true,
+        hotspot: true,
       },
     },
     {
-      name: 'navLinks',
+      name: 'address',
+      title: 'Address',
+      type: 'string',
+    },
+    {
+      name: 'phoneNumber',
+      title: 'Phone Number',
+      type: 'string',
+    },
+    {
+      name: 'socialLinks',
+      title: 'Social Media Links',
+      type: 'array',
+      of: [
+        {
+          title: 'Social Link',
+          type: 'object',
+          fields: [
+            {
+              name: 'type',
+              title: 'Platform',
+              type: 'string',
+              options: {
+                list: [
+                  { title: 'Facebook', value: 'facebook' },
+                  { title: 'Twitter', value: 'twitter' },
+                  { title: 'Instagram', value: 'instagram' },
+                  { title: 'YouTube', value: 'youtube' },
+                  // Add other social platforms as needed
+                ],
+              },
+            },
+            {
+              name: 'url',
+              title: 'URL',
+              type: 'url',
+              validation: (Rule) =>
+                Rule.uri({
+                  allowRelative: false,
+                  scheme: ['http', 'https'],
+                }),
+            },
+          ],
+        },
+      ],
+    },
+    {
+      name: 'navigation',
+      title: 'Navigation Links',
       type: 'array',
       of: [
         {
           type: 'object',
           fields: [
             {
-              name: 'navLinkName',
+              name: 'text',
+              title: 'Text',
               type: 'string',
-              title: 'Nav Link Name',
+              validation: (Rule) => Rule.required(),
             },
             {
-              name: 'navLinkPath',
-              type: 'string',
-              title: 'Nav Link Path',
+              name: 'href',
+              title: 'URL',
+              type: 'url',
+              validation: (Rule) =>
+                Rule.uri({
+                  allowRelative: true,
+                  scheme: ['http', 'https', 'mailto', 'tel'],
+                }),
+            },
+            {
+              name: 'dropdownLinks',
+              title: 'Dropdown Links',
+              type: 'array',
+              of: [
+                {
+                  type: 'object',
+                  fields: [
+                    {
+                      name: 'text',
+                      title: 'Text',
+                      type: 'string',
+                      validation: (Rule) => Rule.required(),
+                    },
+                    {
+                      name: 'href',
+                      title: 'URL',
+                      type: 'url',
+                      validation: (Rule) =>
+                        Rule.uri({
+                          allowRelative: true,
+                          scheme: ['http', 'https'],
+                        }),
+                    },
+                  ],
+                },
+              ],
+              options: {
+                collapsible: true, // Allows the editor to collapse/expand the dropdown menu
+              },
             },
           ],
         },
       ],
     },
   ],
+  preview: {
+    select: {
+      title: 'phoneNumber',
+      media: 'logo',
+    },
+    prepare(selection) {
+      const { title, media } = selection
+      return {
+        title: `Header: ${title}`,
+        media,
+      }
+    },
+  },
 }
