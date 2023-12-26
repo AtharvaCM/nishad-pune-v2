c022cfb1cf2c646047df0b14dcd2f89843052350
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2023-12-26 17:22:21 +0530

    - added artile document type

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,11 +1,12 @@
 import { SchemaTypeDefinition } from 'sanity'
 
+import article from './schemas/documents/article'
 import footer from './schemas/documents/footer'
 import header from './schemas/documents/header'
 import homepage from './schemas/documents/homepage'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [header, footer, homepage],
+  types: [article, header, footer, homepage],
 }
 
 // TODO: Add meta tags in next sprint
