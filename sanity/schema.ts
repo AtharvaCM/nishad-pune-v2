52a4b53f7185d4277c7d85967ce92823dce5fa78
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2023-12-26 17:39:48 +0530

    - Added audienceFeedback document type

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,11 +1,12 @@
 import { SchemaTypeDefinition } from 'sanity'
 
+import audienceFeedback from './schemas/documents/audienceFeedback'
 import footer from './schemas/documents/footer'
 import header from './schemas/documents/header'
 import homepage from './schemas/documents/homepage'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [header, footer, homepage],
+  types: [audienceFeedback, header, footer, homepage],
 }
 
 // TODO: Add meta tags in next sprint
