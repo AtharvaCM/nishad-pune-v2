5e0e986e8f67e2e2b9197a443e3d21eb1c3af44b
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-01-07 14:46:45 +0530

    - Added galley section schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -2,11 +2,12 @@ import { SchemaTypeDefinition } from 'sanity'
 
 import event from './schemas/documents/event'
 import footer from './schemas/documents/footer'
+import galleySection from './schemas/documents/gallerySection'
 import header from './schemas/documents/header'
 import homepage from './schemas/documents/homepage'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [event, footer, header, homepage],
+  types: [event, footer, galleySection, header, homepage],
 }
 
 // TODO: Add meta tags in next sprint
