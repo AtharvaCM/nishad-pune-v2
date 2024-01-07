7316de5e537e9293ff1174386d79f2924b072dc3
AtharvaCM <60681622+AtharvaCM@users.noreply.github.com>
2024-01-07 15:40:46 +0530

    [NP-34] CMS: Create and Manage Show Listings in Sanity (#17)
    * - Added show schema
    
    * - Fixed ticketTier erorr
    
    * - Fix theme validation
    
    ---------
    
    Co-authored-by: Atharva-CM <atharva.mahamuni@bluepineapple.io>

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -4,9 +4,10 @@ import event from './schemas/documents/event'
 import footer from './schemas/documents/footer'
 import header from './schemas/documents/header'
 import homepage from './schemas/documents/homepage'
+import show from './schemas/documents/show'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [event, footer, header, homepage],
+  types: [event, footer, header, homepage, show],
 }
 
 // TODO: Add meta tags in next sprint
