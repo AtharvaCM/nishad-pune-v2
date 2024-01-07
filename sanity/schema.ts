d5f94bee82cda4e081d013ce5d041ec15dc0c292
Atharva <112168596+Atharva-CM@users.noreply.github.com>
2024-01-07 13:22:10 +0530

    [NP-47] CMS: Create Schema for Managing Upcoming Event Information in Sanity (#16)
    * - Added event schema
    
    * - add ticketTier object type

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,11 +1,12 @@
 import { SchemaTypeDefinition } from 'sanity'
 
+import event from './schemas/documents/event'
 import footer from './schemas/documents/footer'
 import header from './schemas/documents/header'
 import homepage from './schemas/documents/homepage'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [header, footer, homepage],
+  types: [event, footer, header, homepage],
 }
 
 // TODO: Add meta tags in next sprint
