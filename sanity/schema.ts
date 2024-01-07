2f995039af164e5b730a67a72980e2918936ac3a
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-01-07 14:11:35 +0530

    - Added to schema

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -5,9 +5,10 @@ import event from './schemas/documents/event'
 import footer from './schemas/documents/footer'
 import header from './schemas/documents/header'
 import homepage from './schemas/documents/homepage'
+import mebershipForm from './schemas/documents/membershipForm'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [audienceFeedback, event, header, footer, homepage],
+  types: [audienceFeedback, event, header, footer, homepage, mebershipForm],
 }
 
 // TODO: Add meta tags in next sprint
