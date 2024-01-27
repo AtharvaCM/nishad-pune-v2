69acca4411cf669a620e73af14a3634f12eb130c
Atharva <112168596+Atharva-CM@users.noreply.github.com>
2024-01-27 12:30:23 +0530

    [NP-52] CMS: Create Schema for Audience Feedback in Sanity (#20)
    - Added audienceFeedback document type

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,6 +1,7 @@
 import { SchemaTypeDefinition } from 'sanity'
 
 import article from './schemas/documents/article'
+import audienceFeedback from './schemas/documents/audienceFeedback'
 import event from './schemas/documents/event'
 import footer from './schemas/documents/footer'
 import galleySection from './schemas/documents/gallerySection'
@@ -9,7 +10,16 @@ import homepage from './schemas/documents/homepage'
 import show from './schemas/documents/show'
 
 export const schema: { types: SchemaTypeDefinition[] } = {
-  types: [article, event, footer, galleySection, header, homepage, show],
+  types: [
+    article,
+    audienceFeedback,
+    event,
+    footer,
+    galleySection,
+    header,
+    homepage,
+    show,
+  ],
 }
 
 // TODO: Add meta tags in next sprint
---
+++ sanity/schemas/documents/audienceFeedback.js
@@ -0,0 +1,68 @@
+/* eslint-disable import/no-anonymous-default-export */
+
+export default {
+  name: 'audienceFeedback',
+  title: 'Audience Feedback',
+  type: 'document',
+  fields: [
+    {
+      name: 'authorName',
+      title: 'Author Name',
+      type: 'string',
+      validation: (Rule) => Rule.required(),
+    },
+    {
+      name: 'testimonial',
+      title: 'Testimonial Text',
+      type: 'text',
+      validation: (Rule) => Rule.max(2000), // Optional: adjust the max length as needed
+    },
+    {
+      name: 'youtubeLink',
+      title: 'YouTube Video Link',
+      type: 'url',
+      validation: (Rule) =>
+        Rule.uri({
+          allowRelative: false,
+          scheme: ['https'],
+        }).optional(),
+    },
+    {
+      name: 'relatedShow',
+      title: 'Related Show/Event',
+      type: 'reference',
+      to: [{ type: 'show' }, { type: 'event' }], // Assuming 'show' is the name of your shows/events schema
+      description: 'Link this feedback to a specific show or event.',
+    },
+    {
+      name: 'isApproved',
+      title: 'Approved',
+      description:
+        'Moderators can toggle this once the feedback is reviewed and approved.',
+      type: 'boolean',
+      validation: (Rule) => Rule.required(),
+    },
+    // Add any other relevant fields as needed
+  ],
+  orderings: [
+    {
+      title: 'Approval Date, New',
+      name: 'approvalDateDesc',
+      by: [{ field: '_updatedAt', direction: 'desc' }],
+    },
+    // Add other orderings as necessary
+  ],
+  preview: {
+    select: {
+      title: 'authorName',
+      subtitle: 'testimonial',
+    },
+    prepare(selection) {
+      const { title, subtitle } = selection
+      return {
+        title: title,
+        subtitle: subtitle,
+      }
+    },
+  },
+}
