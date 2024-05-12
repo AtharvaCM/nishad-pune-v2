6b033376713daf6a713d217d58e836f7fc933f29
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-05-12 20:01:00 +0530

    feat: add query fetching example

--- components/common/header/index.tsx
+++ components/common/header/index.tsx
@@ -1,5 +1,14 @@
+import { client } from '@/sanity/lib/client';
+import { FETCH_HEADER } from '@/sanity/queries/header/fetch-header';
+import { FETCH_HEADERResult } from '@/types/generated/sanity.types';
+
 export interface IHeaderProps {}
 
-export default function Header(_props: IHeaderProps) {
-  return <div></div>
+export default async function Header(_props: IHeaderProps) {
+  const headerData = await client.fetch<FETCH_HEADERResult>(FETCH_HEADER);
+  // FIXME: Remove this log after done with the example
+  // eslint-disable-next-line no-console
+  console.log('headerData: ', headerData);
+
+  return <header></header>;
 }
