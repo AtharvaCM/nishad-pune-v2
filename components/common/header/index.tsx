f32f70c0a33e2c494fcef79e415a830b3a35f2a3
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-05-30 23:14:10 +0530

    feat: convert tialwind classes to module css

--- components/common/header/index.tsx
+++ components/common/header/index.tsx
@@ -1,4 +1,6 @@
 'use client';
+import cx from 'classnames';
+import Image from 'next/image';
 import Link from 'next/link';
 import React, { useEffect, useState } from 'react';
 import { FaBars, FaTimes } from 'react-icons/fa';
@@ -7,7 +9,8 @@ import { client } from '@/sanity/lib/client';
 import { FETCH_HEADER } from '@/sanity/queries/header/fetch-header';
 import { FETCH_HEADERResult } from '@/types/generated/sanity.types';
 
-import { fetchImageURL } from './../../../utils/functions/fetchImageURL';
+import { fetchImageURL } from '../../../utils/functions/fetchImageURL';
+import styles from './header.module.scss';
 
 export interface IHeaderProps {}
 
@@ -37,30 +40,28 @@ export default function Header(_props: IHeaderProps) {
   };
 
   return (
-    <nav className="bg-white shadow-md">
-      <div className="max-w-7xl mx-auto px-3 sm:px-6 lg:px-1">
-        <div className="flex justify-between h-16">
-          <div className="flex items-center">
-            <div className="flex-shrink-0">
-              <Link href="/">{logoUrl && <img className="h-12 w-12 p-1" src={logoUrl} alt="Logo" />}</Link>
+    <nav className={cx(styles['d-container'])}>
+      <div className={cx(styles['d-container__inner'])}>
+        <div className={cx(styles['d-container__content'])}>
+          <div className={cx(styles['d-container__logo-container'])}>
+            <div className={cx(styles['d-container__logo'])}>
+              <Link href="/">
+                {logoUrl && <Image className={cx(styles['d-container__logo-img'])} src={logoUrl} alt="Logo" width={48} height={48} />}
+              </Link>
             </div>
           </div>
-          <div className="hidden md:flex md:items-center md:ml-auto md:space-x-8">
+          <div className={cx(styles['d-container__navigation'])}>
             {headerData?.navigation?.map(({ text, href }) => (
-              <Link
-                key={text}
-                href={`${href?.toString()}`}
-                className="text-gray-800 hover:text-gray-600 px-3 py-2 rounded-md text-sm font-medium"
-              >
+              <Link key={text} href={`${href?.toString()}`} className={cx(styles['d-container__navigation-link'])}>
                 {text}
               </Link>
             ))}
           </div>
-          <div className="flex items-center md:hidden">
+          <div className={cx(styles['d-container__menu-button-container'])}>
             <button
               onClick={toggleMenu}
               type="button"
-              className="bg-gray-100 inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-gray-300"
+              className={cx(styles['d-container__menu-button'])}
               aria-controls="mobile-menu"
               aria-expanded="false"
             >
@@ -70,14 +71,10 @@ export default function Header(_props: IHeaderProps) {
         </div>
       </div>
 
-      <div className={`${isOpen ? 'block' : 'hidden'} md:hidden`} id="mobile-menu">
-        <div className="px-2 pt-2 pb-3 space-y-1 sm:px-3">
+      <div className={cx(styles['d-container__mobile-menu'], { [styles['d-container__mobile-menu--open']]: isOpen })} id="mobile-menu">
+        <div className={cx(styles['d-container__mobile-menu-content'])}>
           {headerData?.navigation?.map(({ text, href }) => (
-            <Link
-              key={text}
-              href={`${href?.toString()}`}
-              className="text-gray-800 hover:text-gray-600 block px-3 py-2 rounded-md text-base font-medium"
-            >
+            <Link key={text} href={`${href?.toString()}`} className={cx(styles['d-container__mobile-menu-link'])}>
               {text}
             </Link>
           ))}
