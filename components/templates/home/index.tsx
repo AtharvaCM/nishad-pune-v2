18ac73f48e0aa7290130ba68db5ff99c6a2566a9
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-06-12 15:50:56 +0530

    feat: add slick slider

--- components/templates/home/index.tsx
+++ components/templates/home/index.tsx
@@ -1,4 +1,7 @@
+'use client';
+
 import HeroSection from '@/components/home/hero-section';
+import SimpleSlider from '@/components/test-slider';
 
 export interface IHomePageTemplateProps {}
 
@@ -6,6 +9,7 @@ export default function HomePageTemplate(_props: IHomePageTemplateProps) {
   return (
     <div>
       <HeroSection />
+      <SimpleSlider />
     </div>
   );
 }
