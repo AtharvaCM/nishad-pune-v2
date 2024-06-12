b9c92c1cedac944be749357f52caed7f86e8a5dd
Atharva-CM <atharva.mahamuni@bluepineapple.io>
2024-06-12 15:31:24 +0530

    fix: sonarlint error

--- components/Post.tsx
+++ components/Post.tsx
@@ -1,14 +1,14 @@
-import { PortableText } from '@portabletext/react'
-import imageUrlBuilder from '@sanity/image-url'
-import Image from 'next/image'
-import { SanityDocument } from 'next-sanity'
+import { PortableText } from '@portabletext/react';
+import imageUrlBuilder from '@sanity/image-url';
+import Image from 'next/image';
+import { SanityDocument } from 'next-sanity';
 
-import { dataset, projectId } from '@/sanity/env'
+import { dataset, projectId } from '@/sanity/env';
 
-const builder = imageUrlBuilder({ projectId, dataset })
+const builder = imageUrlBuilder({ projectId, dataset });
 
-export default function Post({ post }: { post: SanityDocument }) {
-  const { title, mainImage, body } = post
+export default function Post({ post }: Readonly<{ post: SanityDocument }>) {
+  const { title, mainImage, body } = post;
 
   return (
     <main className="container prose prose-lg mx-auto p-4">
@@ -16,12 +16,7 @@ export default function Post({ post }: { post: SanityDocument }) {
       {mainImage ? (
         <Image
           className="float-left m-0 mr-4 w-1/3 rounded-lg"
-          src={builder
-            .image(mainImage)
-            .width(300)
-            .height(300)
-            .quality(80)
-            .url()}
+          src={builder.image(mainImage).width(300).height(300).quality(80).url()}
           width={300}
           height={300}
           alt={mainImage.alt || ''}
@@ -29,5 +24,5 @@ export default function Post({ post }: { post: SanityDocument }) {
       ) : null}
       {body ? <PortableText value={body} /> : null}
     </main>
-  )
+  );
 }
