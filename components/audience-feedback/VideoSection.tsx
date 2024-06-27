6a2c401f6c3494c725e2dd778fbef91f544f512a
Shubham Mankar <mankarshubham81@gmail.com>
2024-06-27 11:37:20 +0530

    feat: new layout

--- components/audience-feedback/VideoSection.tsx
+++ components/audience-feedback/VideoSection.tsx
@@ -4,42 +4,32 @@ import VideoEmbed from './VideoEmbed';
 
 const videoData = [
   {
-    isVideo: true,
     videoId: 'dQw4w9WgXcQ',
     title: 'Ready to Accelerate?',
     author: 'Shawn Hesketh',
-  },
-  {
-    isVideo: false,
     info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam blanditiis eius esse modi similique voluptatibus repellat tenetur, repudiandae placeat officia? Voluptatibus, ipsum?',
   },
   {
-    isVideo: true,
     videoId: '3GwjfUFyY6M',
     title: 'No Cookie-Cutter Advice',
     author: 'Joe Casabona',
-  },
-  {
-    isVideo: false,
     info: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam blanditiis eius esse modi similique voluptatibus repellat tenetur, repudiandae placeat officia? Voluptatibus, ipsum?',
   },
 ];
 
 const VideoSection: React.FC = () => (
   <div className="container mx-auto my-8 px-8">
-    <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6">
-      {videoData.map((video, index) =>
-        video.isVideo ? (
-          <VideoEmbed key={index} videoId={`${video.videoId}`} title={`${video.title}`} author={`${video.author}`} />
-        ) : (
-          <div key={index} className={'w-full p-8 flex items-center justify-center text-center'}>
-            <span className="text-gray-600">
-              <h2 className="font-semibold">Ready to Accelerate?</h2>
-              {video.info}
-            </span>
-          </div>
-        ),
-      )}
+    <div>
+      {videoData.map((video, index) => (
+        <VideoEmbed
+          key={index}
+          videoId={`${video.videoId}`}
+          title={`${video.title}`}
+          author={`${video.author}`}
+          info={`${video.info}`}
+          bgGray={index % 2 === 0 ? 'bg-gray-200' : 'bg-white'}
+        />
+      ))}
     </div>
   </div>
 );
